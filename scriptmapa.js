0cb009d3e0421d2066a3c7319508d3707eb23f09
AaronidkNOT <133139702+AaronidkNOT@users.noreply.github.com>
2025-07-10 21:27:17 -0300

    Add files via upload

---
+++ scriptmapa.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const mainTitle = document.querySelector('.main-title');
+    const originalText = mainTitle.textContent;
+    mainTitle.textContent = '';
+
+    let i = 0;
+    function typeWriter() {
+        if (i < originalText.length) {
+            mainTitle.textContent += originalText.charAt(i);
+            i++;
+            setTimeout(typeWriter, 120);
+        }
+    }
+    setTimeout(typeWriter, 500);
+
+    const mapPreview = document.getElementById('mapPreview');
+    if (mapPreview) {
+        setTimeout(() => {
+            mapPreview.classList.add('show');
+        }, 2000);
+    }
+});
